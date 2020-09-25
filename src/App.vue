6aace619a0b114fd7f896b0b0b6ae1a2c3c1a57d
fanfan328 <stef.flamez97@gmail.com>
2020-09-25 15:43:43 +0700

    Manage Token dan Login

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
 
 <script>
   import headerMenu from './components/headerMenu.vue'
+  import axios from "axios";
 
 export default {
   name: 'App',
@@ -32,6 +33,65 @@ export default {
     };
   },
   methods: {
+    refreshAccessToken(){
+      const options = {
+        url: "http://localhost:8000/api/refresh",
+        method: "post",
+        headers: {
+            'Authorization': 'Bearer '+this.$session.get('token')
+        }
+        };
+      
+      axios(options)
+        .then(response => {
+          this.$session.set('token', response.data.token);
+        })
+        .catch(e => {
+          // 
+            alert(e + "Failed to Refresh Token");
+            this.$router.push('/');
+            this.$session.destroy();
+        });
+
+        
+    }
+  },
+  created: function () {
+    axios.interceptors.request.use(config => {
+        console.log('Req Intercept'+JSON.stringify(config));
+        return config
+      }, error => Promise.reject(error))
+
+  
+    axios.interceptors.response.use(response => {
+      console.log('Res Intercept'+JSON.stringify(response));
+      return response
+    }, error =>{
+      const originalRequest = error.config;
+
+      // console.log(JSON.stringify(error.response.data.refresh.token));
+      if(error.response.status==401){
+        //Set New Token
+        let token=error.response.data.refresh.token
+        this.$session.set('token', token);
+        // console.log('OLD ' + JSON.stringify(originalRequest));
+        
+        // axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+        // axios.defaults['Authorization'] = 'Bearer ' + token;
+        originalRequest.headers['Authorization']='Bearer ' + token;
+
+        console.log('NEW ' + JSON.stringify(originalRequest));
+        return axios(originalRequest);
+      }else{
+        alert("Failed to Refresh Token");
+        this.$router.push('/');
+        this.$session.destroy();
+
+        return Promise.reject(error)
+      }
+      // this.cover = false
+    })
+    
   }
 };
 </script>
